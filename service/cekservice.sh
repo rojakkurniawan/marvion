fc170cbe800c3c851ec0fa5baf11aabb7f39727d
Rojak Kurniawan <rojakkurniawan4@gmail.com>
2025-02-01 17:52:41 +0700

    Refactor cekservice.sh: Improve code structure and function modularity
    - Restructure script with modular functions for status checks
    - Simplify variable assignments and function calls
    - Remove redundant code and improve readability
    - Maintain existing functionality while enhancing code organization

--- service/cekservice.sh
+++ service/cekservice.sh
@@ -1,87 +1,84 @@
 #!/bin/bash
+
+# Mengambil domain dan token dari file konfigurasi
 domain=$(cat /etc/data/domain)
 token=$(cat /etc/data/token.json | jq -r .access_token)
 
-# // Code for service
-export RED='\033[0;31m';
-export GREEN='\033[0;32m';
-export YELLOW='\033[0;33m';
-export BLUE='\033[0;34m';
-export PURPLE='\033[0;35m';
-export CYAN='\033[0;36m';
-export LIGHT='\033[0;37m';
-export NC='\033[0m';
+export RED='\033[0;31m'
+export GREEN='\033[0;32m'
+export YELLOW='\033[0;33m'
+export BLUE='\033[0;34m'
+export PURPLE='\033[0;35m'
+export CYAN='\033[0;36m'
+export LIGHT='\033[0;37m'
+export NC='\033[0m'
 
-# // Export Banner Status Information
-export ERROR="[${RED} ERROR ${NC}]";
-export INFO="[${YELLOW} INFO ${NC}]";
-export OKEY="[${GREEN} OKEY ${NC}]";
-export PENDING="[${YELLOW} PENDING ${NC}]";
-export SEND="[${YELLOW} SEND ${NC}]";
-export RECEIVE="[${YELLOW} RECEIVE ${NC}]";
+export ERROR="[${RED} ERROR ${NC}]"
+export INFO="[${YELLOW} INFO ${NC}]"
+export OKEY="[${GREEN} OKEY ${NC}]"
+export PENDING="[${YELLOW} PENDING ${NC}]"
+export SEND="[${YELLOW} SEND ${NC}]"
+export RECEIVE="[${YELLOW} RECEIVE ${NC}]"
 
-# // VAR
-if [[ $(netstat -ntlp | grep -i nginx | grep -i 0.0.0.0:443 | awk '{print $4}' | cut -d: -f2 | xargs | sed -e 's/ /, /g') == '443' ]]; then
-    NGINX="${GREEN}Okay${NC}";
-else
-    NGINX="${RED}Not Okay${NC}";
-fi
+check_nginx_status() {
+    if [[ $(netstat -ntlp | grep -i nginx | grep -i 0.0.0.0:443 | awk '{print $4}' | cut -d: -f2 | xargs | sed -e 's/ /, /g') == '443' ]]; then
+        echo "${GREEN}Okay${NC}"
+    else
+        echo "${RED}Not Okay${NC}"
+    fi
+}
 
-if [[ $(netstat -ntlp | grep -i python | grep -i "127.0.0.1:8000" | awk '{print $4}' | cut -d: -f2 | xargs | sed -e 's/ /, /g') == "8000" ]]; then
-    MARZ="${GREEN}Okay${NC}";
-else
-    MARZ="${RED}Not Okay${NC}";
-fi
+check_marzban_status() {
+    if [[ $(netstat -ntlp | grep -i python | grep -i "127.0.0.1:8000" | awk '{print $4}' | cut -d: -f2 | xargs | sed -e 's/ /, /g') == "8000" ]]; then
+        echo "${GREEN}Okay${NC}"
+    else
+        echo "${RED}Not Okay${NC}"
+    fi
+}
 
-if [[ $(systemctl status ufw | grep -w Active | awk '{print $2}' | sed 's/(//g' | sed 's/)//g' | sed 's/ //g') == 'active' ]]; then
-    UFW="${GREEN}Okay${NC}";
-else
-    UFW="${RED}Not Okay${NC}";
-fi
-
-# Function to fetch system information from Marzban API
-function get_marzban_info() {
-
+check_ufw_status() {
+    if [[ $(systemctl status ufw | grep -w Active | awk '{print $2}' | sed 's/(//g' | sed 's/)//g' | sed 's/ //g') == 'active' ]]; then
+        echo "${GREEN}Okay${NC}"
+    else
+        echo "${RED}Not Okay${NC}"
+    fi
+}
+get_marzban_info() {
     local marzban_api="https://${domain}/api/system"
     local marzban_info=$(curl -s -X 'GET' "$marzban_api" -H 'accept: application/json' -H "Authorization: Bearer $token")
 
     if [[ $? -eq 0 ]]; then
-# Parsing Marzban API response
-marzban_version=$(echo "$marzban_info" | jq -r '.version')
+        echo "$marzban_info" | jq -r '.version'
     else
         echo -e "${ERROR} Failed to fetch Marzban information."
         exit 1
     fi
 }
 
-# Usage of the function
-get_marzban_info "$domain" "$token"
-
-# Function to get Xray Core version
-function get_xray_core_version() {
-    xray_core_info=$(curl -s -X 'GET' \
+get_xray_core_version() {
+    local xray_core_info=$(curl -s -X 'GET' \
         "https://${domain}/api/core" \
         -H 'accept: application/json' \
-        -H "Authorization: Bearer ${token}"
-    )
-    xray_core_version=$(echo "$xray_core_info" | jq -r '.version')
-
-    echo "$xray_core_version"
+        -H "Authorization: Bearer ${token}")
+    echo "$xray_core_info" | jq -r '.version'
 }
 
-# Get Xray Core version
-xray_core_version=$(get_xray_core_version "$domain" "$token")
+NGINX=$(check_nginx_status)
+MARZ=$(check_marzban_status)
+UFW=$(check_ufw_status)
+marzban_version=$(get_marzban_info)
+xray_core_version=$(get_xray_core_version)
 
 echo ""
-echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
 echo -e "\E[42;1;39m            ⇱ Service Information ⇲             \E[0m"
-echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
 echo -e "❇️ Marzban Version     : ${GREEN}${marzban_version}${NC}"
 echo -e "❇️ XrayCore Version    : ${GREEN}${xray_core_version}${NC}"
 echo -e "❇️ Nginx               : $NGINX"
 echo -e "❇️ Firewall            : $UFW"
 echo -e "❇️ Marzban Panel       : $MARZ"
-echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
-echo -e "                          MARZBAN NGINX PORT 443"
-echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+echo -e "               MARZBAN NGINX PORT 443"
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
 echo ""
